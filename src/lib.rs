dee3820983afc6108d860da387222a38833e0e3a
Andrew Smith <andrew.smith@neworbit.org>
2021-03-29 07:57:35 +0100

    Watch the user status file

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,29 @@
 use dirs;
+use hotwatch::blocking::{Flow, Hotwatch};
+use hotwatch::Event;
+use std::fs;
 use std::path::PathBuf;
+use std::time::Duration;
 
 pub fn run() {
-    println!("Status file path: {:?}", status_file_path());
+    let status_file_path = status_file_path();
+    println!("Status file path: {:?}", status_file_path);
+
+    let mut hotwatch = Hotwatch::new_with_custom_delay(Duration::from_millis(100))
+        .expect("File watcher failed to initialize");
+
+    hotwatch
+        .watch(status_file_path, |event: Event| {
+            if let Event::Write(path) = event {
+                println!("{:?} has changed", path);
+                let status = fs::read_to_string(path).unwrap();
+                println!("{}", status);
+            }
+            Flow::Continue
+        })
+        .expect("Failed to watch status file");
+
+    hotwatch.run();
 }
 
 fn status_file_path() -> PathBuf {
