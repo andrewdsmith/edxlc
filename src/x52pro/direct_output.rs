e6af3be16acd8af3a8c05ba2333a1e115b4acba3
Andrew Smith <andrew.smith@neworbit.org>
2021-04-11 09:28:17 +0100

    Remove `DirectOuput` prefix on types in module

--- src/x52pro/direct_output.rs
+++ src/x52pro/direct_output.rs
@@ -7,23 +7,20 @@ use winapi::ctypes::wchar_t;
 use winapi::shared::minwindef::DWORD;
 use winapi::um::winnt::HRESULT;
 
-type DirectOutputDeviceHandle = *const c_void;
+type DeviceHandle = *const c_void;
 
-type DirectOutputInitializeFn = unsafe extern "C" fn(wszPluginName: *const wchar_t) -> HRESULT;
-type DirectOutputEnumerateFn = unsafe extern "C" fn(
-    pfnCb: DirectOutputEnumerateCallbackFn,
-    pCtxt: &mut DirectOutput,
-) -> HRESULT;
-type DirectOutputEnumerateCallbackFn =
-    extern "C" fn(hDevice: DirectOutputDeviceHandle, pCtxt: &mut DirectOutput);
-type DirectOutputAddPageFn = unsafe extern "C" fn(
-    hDevice: DirectOutputDeviceHandle,
+type InitializeFn = unsafe extern "C" fn(wszPluginName: *const wchar_t) -> HRESULT;
+type EnumerateFn =
+    unsafe extern "C" fn(pfnCb: EnumerateCallbackFn, pCtxt: &mut DirectOutput) -> HRESULT;
+type EnumerateCallbackFn = extern "C" fn(hDevice: DeviceHandle, pCtxt: &mut DirectOutput);
+type AddPageFn = unsafe extern "C" fn(
+    hDevice: DeviceHandle,
     dwPage: DWORD,
     wszDebugName: *const wchar_t,
     dwFlags: DWORD,
 ) -> HRESULT;
-type DirectOutputSetLedFn = unsafe extern "C" fn(
-    hDevice: DirectOutputDeviceHandle,
+type SetLedFn = unsafe extern "C" fn(
+    hDevice: DeviceHandle,
     dwPage: DWORD,
     dwIndex: DWORD,
     dwValue: DWORD,
@@ -36,7 +33,7 @@ const PAGE_ID: DWORD = 1;
 
 pub struct DirectOutput {
     library: Library,
-    device: DirectOutputDeviceHandle,
+    device: DeviceHandle,
 }
 
 impl DirectOutput {
@@ -57,8 +54,8 @@ impl DirectOutput {
     pub fn initialize(&self) {
         unsafe {
             let initialize_fn =
-                self.load_library_function::<DirectOutputInitializeFn>(b"DirectOutput_Initialize");
-            let result = initialize_fn(DirectOutput::win32_string(PLUGIN_NAME).as_ptr());
+                self.load_library_function::<InitializeFn>(b"DirectOutput_Initialize");
+            let result = initialize_fn(Self::win32_string(PLUGIN_NAME).as_ptr());
             println!("DirectOutput_Initialize result = {:?}", result);
 
             if result != 0 {
@@ -68,14 +65,13 @@ impl DirectOutput {
     }
 
     pub fn enumerate(&mut self) {
-        extern "C" fn callback(device: DirectOutputDeviceHandle, target: &mut DirectOutput) {
+        extern "C" fn callback(device: DeviceHandle, target: &mut DirectOutput) {
             println!("DirectOutput_Enumerate device = {:?}", device);
             target.device = device;
         }
 
         unsafe {
-            let enumerate_fn =
-                self.load_library_function::<DirectOutputEnumerateFn>(b"DirectOutput_Enumerate");
+            let enumerate_fn = self.load_library_function::<EnumerateFn>(b"DirectOutput_Enumerate");
             let result = enumerate_fn(callback, self);
             println!("DirectOutput_Enumerate result = {:?}", result);
 
@@ -91,8 +87,7 @@ impl DirectOutput {
         let debug_name = DirectOutput::win32_string(PLUGIN_NAME).as_ptr();
 
         unsafe {
-            let add_page_fn =
-                self.load_library_function::<DirectOutputAddPageFn>(b"DirectOutput_AddPage");
+            let add_page_fn = self.load_library_function::<AddPageFn>(b"DirectOutput_AddPage");
             let result = add_page_fn(self.device, PAGE_ID, debug_name, FLAG_SET_AS_ACTIVE);
             println!("DirectOutput_AddPage result = {:?}", result);
 
@@ -107,8 +102,7 @@ impl DirectOutput {
 
         unsafe {
             // We should not be re-loading this function symbol on every call.
-            let set_led_fn =
-                self.load_library_function::<DirectOutputSetLedFn>(b"DirectOutput_SetLed");
+            let set_led_fn = self.load_library_function::<SetLedFn>(b"DirectOutput_SetLed");
             let result = set_led_fn(self.device, PAGE_ID, id, value);
             println!("DirectOutput_SetLed result = {:?}", result);
 
