bd586cbb45a805c80ef769d008b53a1bc2a0a9ee
Andrew Smith <andrew.smith@neworbit.org>
2021-04-20 08:10:28 +0100

    Move Ship constants to module level
    This change is in preparation for adding tests, which will need access
    to these constants.

--- src/game.rs
+++ src/game.rs
@@ -4,19 +4,18 @@ pub mod file;
 pub use controls::*;
 use file::Status;
 
+// See: https://elite-journal.readthedocs.io/en/latest/Status%20File/
+const LANDING_GEAR_DEPLOYED: u32 = 1 << 2;
+const EXTERNAL_LIGHTS_ON: u32 = 1 << 8;
+const CARGO_SCOOP_DEPLOYED: u32 = 1 << 9;
+
+const STATUS_FILTER: u32 = LANDING_GEAR_DEPLOYED | CARGO_SCOOP_DEPLOYED | EXTERNAL_LIGHTS_ON;
+
 pub struct Ship {
     status_flags: u32,
 }
 
 impl Ship {
-    // See: https://elite-journal.readthedocs.io/en/latest/Status%20File/
-    const LANDING_GEAR_DEPLOYED: u32 = 1 << 2;
-    const EXTERNAL_LIGHTS_ON: u32 = 1 << 8;
-    const CARGO_SCOOP_DEPLOYED: u32 = 1 << 9;
-
-    const STATUS_FILTER: u32 =
-        Self::LANDING_GEAR_DEPLOYED | Self::CARGO_SCOOP_DEPLOYED | Self::EXTERNAL_LIGHTS_ON;
-
     pub fn from_status(status: Status) -> Self {
         Self {
             status_flags: Self::filtered_status_flags(status.flags),
@@ -35,15 +34,15 @@ impl Ship {
     }
 
     pub fn landing_gear_deployed(&self) -> bool {
-        self.is_status_flag_set(Self::LANDING_GEAR_DEPLOYED)
+        self.is_status_flag_set(LANDING_GEAR_DEPLOYED)
     }
 
     pub fn cargo_scoop_deployed(&self) -> bool {
-        self.is_status_flag_set(Self::CARGO_SCOOP_DEPLOYED)
+        self.is_status_flag_set(CARGO_SCOOP_DEPLOYED)
     }
 
     pub fn external_lights_on(&self) -> bool {
-        self.is_status_flag_set(Self::EXTERNAL_LIGHTS_ON)
+        self.is_status_flag_set(EXTERNAL_LIGHTS_ON)
     }
 
     fn is_status_flag_set(&self, flag: u32) -> bool {
@@ -51,6 +50,6 @@ impl Ship {
     }
 
     fn filtered_status_flags(flags: u32) -> u32 {
-        flags & Self::STATUS_FILTER
+        flags & STATUS_FILTER
     }
 }
